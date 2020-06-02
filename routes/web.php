9bc29f849a196aa8d7e8b14d6b629aa847e7500c
Dima Antonenko <daemondw@gmail.com>
2020-06-02 14:28:29 +0200

    ORM

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,6 @@
 <?php
+use App\Task;
 
-use Illuminate\Support\Facades\Route;
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-Route::get('/tasks', function () {
-
-    $tasks = App\Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
